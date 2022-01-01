2d0e432c6744738264057bba00b5a3629bc46fad
jasem-elwaar <jassem.elwaar@gmail.com>
2022-01-02 00:08:43 +0100

    Starting

---
+++ twitter_clone/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1> hello there  </h1>  
+    {/* Sidebar */}
+
+    {/* Feed */}
+    
+    {/* Widget */}
+    
+    </div>
+  );
+}
+
+export default App;
